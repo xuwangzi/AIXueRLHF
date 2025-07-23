99e22f5c26ff24d2af9fc51d1631417bb8527fdf
jiangrc22 <jiangrc22@mails.tsinghua.edu.cn>
2025-07-22 20:42:46 -0700

    upgrade accelerate & deepspeed

--- scripts/test_deepspeed_zero3_debug.sh
+++ scripts/test_deepspeed_zero3_debug.sh
@@ -1,13 +1,14 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
+export NCCL_DEBUG=INFO
 
 accelerate launch --config_file configs/deepspeed_zero3.yaml \
     --main_process_port 29504 \
     src/aixue.py \
     --dataset_name data/aixue_test_data \
-    --output_dir /root/group-shared/jrc/ppo-test/models/4gpu_aixue_data_test \
+    --output_dir /root/group-shared/jrc/ppo-test/models/deepspeed_test_larger_batch \
     --learning_rate 3e-6 \
     --per_device_train_batch_size 2 \
-    --gradient_accumulation_steps 4 \
+    --gradient_accumulation_steps 60 \
     --total_episodes 960 \
     --model_name_or_path /root/group-shared/jrc/base-models/Qwen3-32B \
     --sft_model_path /root/group-shared/jrc/base-models/Qwen3-32B \
@@ -17,4 +18,4 @@ accelerate launch --config_file configs/deepspeed_zero3.yaml \
     --report_to tensorboard \
     --save_safetensors true \
     --attn_implementation flash_attention_2 \
-    2>&1 | tee -a /root/group-shared/jrc/ppo-test/logs/4gpu_data_test.log
+    2>&1 | tee -a /root/group-shared/jrc/ppo-test/logs/deepspeed_test_larger_batch.log
